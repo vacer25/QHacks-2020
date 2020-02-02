da09f505ab0097e2e1e8ad56d6718f8d0247cb2c
AlexKovalev <rubiksType42!>
2020-02-02 03:09:49 -0500

    made text look at camera and generated text using script

---
+++ ARFaceInfo/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
---
+++ ARFaceInfo/Assets/Scripts/PutMessenger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+    public class PutMessenger : MonoBehaviour
+{
+    public TextMeshPro textBox;
+
+    readonly string getURL = "https://us-central1-affable-armor-266916.cloudfunctions.net/hello-world-function";
+    void Start()
+    {
+         StartCoroutine(SimpleGetRequest());
+    }
+
+    IEnumerator SimpleGetRequest()
+    {
+        byte[] myData = System.Text.Encoding.UTF8.GetBytes("This is some test data");
+
+        UnityWebRequest www = UnityWebRequest.Put(getURL, myData);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            string result = www.downloadHandler.text;
+            textBox.text = result;
+        }
+    }
+}
